9eebd0e16e48644e54b37496aeb00af68ef0d900
Berthier <Berthier.est@gmail.com>
2020-07-29 14:11:53 +0200

    bulk

---
+++ src/main/kotlin/io/kuzzle/sdk/controllers/BulkController.kt
@@ -0,0 +1,92 @@
+package io.kuzzle.sdk.controllers
+
+import io.kuzzle.sdk.Kuzzle
+import io.kuzzle.sdk.coreClasses.maps.KuzzleMap
+import io.kuzzle.sdk.coreClasses.responses.Response
+import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+
+class BulkController(kuzzle: Kuzzle) : BaseController(kuzzle) {
+
+@JvmOverloads
+  fun deleteByQuery(
+      index: String,
+      collection: String,
+      searchQuery: ConcurrentHashMap<String, Any?>,
+      waitForRefresh: Boolean? = null): CompletableFuture<Int> {
+    val query = KuzzleMap().apply {
+      put("index", index)
+      put("collection", collection)
+      put("controller", "bulk")
+      put("action", "deleteByQuery")
+      put("body", searchQuery)
+      put("waitForRefresh", waitForRefresh)
+    }
+    return kuzzle
+        .query(query)
+        .thenApplyAsync { response -> (response.result as ConcurrentHashMap<String?, Any?>)["deleted"] as LazilyParsedNumber).toInt()}
+  }
+
+@JvmOverloads
+  fun import(
+      index: String,
+      collection: String,
+      bulkData: ConcurrentHashMap<String, Any?>,
+      queuable: Boolean? = null): CompletableFuture<ConcurrentHashMap<String, Any?> {
+    val query = KuzzleMap().apply {
+      put("index", index)
+      put("collection", collection)
+      put("controller", "bulk")
+      put("action", "import")
+      put("body", bulkData),
+      put("queuable", queuable)
+    }
+    return kuzzle
+        .query(query)
+        .thenApplyAsync { response -> (response.result as ConcurrentHashMap<String, Any?>) }
+  }
+
+@JvmOverloads
+  fun mWrite(
+      index: String,
+      collection: String,
+      documents: ArrayList<ConcurrentHashMap<String, Any?>>,
+      notify: Boolean? = null,
+      waitForRefresh: Boolean? = null): CompletableFuture<ConcurrentHashMap<String, Any?>> {
+    val query = KuzzleMap().apply {
+      put("index", index)
+      put("collection", collection)
+      put("controller", "bulk")
+      put("action", "mWrite")
+      put("body", documents)
+      put("waitForRefresh", waitForRefresh)
+      put("notify", notify)
+    }
+    return kuzzle
+        .query(query)
+        .thenApplyAsync { response -> (response.result as ConcurrentHashMap<String, Any?>) }
+  }
+
+@JvmOverloads
+  fun write(
+      index: String,
+      collection: String,
+      content: ConcurrentHashMap<String, Any?>,
+      id: String? = null,
+      notify: Boolean? = null,
+      waitForRefresh: Boolean? = null): CompletableFuture<ConcurrentHashMap<String, Any?> {
+    val query = KuzzleMap().apply {
+      put("index", index)
+      put("collection", collection)
+      put("controller", "bulk")
+      put("action", "write")
+      put("body", content)
+      put("waitForRefresh", waitForRefresh)
+      put("notify", notify)
+    }
+    return kuzzle
+        .query(query)
+        .thenApplyAsync { response -> (response.result as ConcurrentHashMap<String, Any?>)) }
+  }
+}
