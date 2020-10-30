e58df57a4086282b202446655d3457c20c9c4f59
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-10-30 14:29:57 +0100

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.gradle.jvm.tasks.Jar
+import com.jfrog.bintray.gradle.BintrayPlugin
+import com.jfrog.bintray.gradle.BintrayExtension
 
 plugins {
     application
     `maven-publish`
     kotlin("jvm") version "1.3.61"
     id("org.jetbrains.dokka") version "0.10.0"
+    id("com.jfrog.bintray") version "1.8.0"
 }
 
 group = "io.kuzzle.sdk"
@@ -76,15 +79,59 @@ tasks.withType<Jar> {
 
 publishing {
     publications {
-        create<MavenPublication>("default") {
+        create<MavenPublication>("MyPublication") {
             from(components["java"])
             artifact(sourcesJar)
             artifact(dokkaJar)
+            groupId("io.kuzzle")
+            artifactId("kuzzle-sdk-jvm")
+            version(version)
+            pom.withXml {
+            def root = asNode()
+            root.appendNode("description", "Kuzzle JVM SDK")
+            root.appendNode("name", "kuzzle-sdk-java")
+            root.appendNode("url", "https://github.com/kuzzleio/sdk-jvm")
+            root.children().last() + pomConfig
+            }
         }
     }
     repositories {
         maven {
             url = uri("$buildDir/repository")
         }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    publish = true
+    setPublications("MyPublication")
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "kuzzle-sdk-jvm"
+        userOrg = "kuzzle"
+        websiteUrl = "https://kuzzle.io"
+        vcsUrl = "https://github.com/kuzzleio/sdk-jvm.git"
+        setLabels("kotlin")
+        desc = "Kuzzle JVM SDK"
+        publicDownloadNumbers = true
+        publish = true
+    })
+
+}
+
+tasks {
+    withType(GradleBuild::class.java) {
+        dependsOn(shadowJar)
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    withType(Test::class.java) {
+        testLogging.showStandardStreams = true
+    }
+    withType<GenerateMavenPom> {
+        destination = file("$buildDir/libs/${shadowJar.archiveName}.pom")
     }
 }
