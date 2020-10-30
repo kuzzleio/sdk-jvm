c286c84ab87c9b199fea1df1a4df2a46a346f45a
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-10-30 15:13:47 +0100

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,99 @@
 import org.gradle.jvm.tasks.Jar
+import java.time.LocalDateTime
 import com.jfrog.bintray.gradle.BintrayPlugin
 import com.jfrog.bintray.gradle.BintrayExtension
+import org.gradle.api.publish.maven.MavenPom
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
     `maven-publish`
     kotlin("jvm") version "1.3.61"
     id("org.jetbrains.dokka") version "0.10.0"
-    id("com.jfrog.bintray") version "1.8.0"
+    id("com.jfrog.bintray") version "1.8.4"
+}
+
+val artifactName = project.name
+val artifactGroup = "io.kuzzle"
+val artifactVersion = project.version.toString()
+
+val pomUrl = "https://github.com/kuzzleio/sdk-jvm"
+val pomScmUrl = "https://github.com/kuzzleio/sdk-jvm"
+val pomIssueUrl = "https://github.com/kuzzleio/sdk-jvm/issues"
+val pomDesc = "https://github.com/kuzzleio/sdk-jvm"
+
+val githubRepo = "kuzzleio/sdk-jvm"
+val githubReadme = "README.md"
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "kuzzleio"
+val pomDeveloperName = "kuzzle"
+
+
+publishing {
+    publications {
+        create<MavenPublication>("kuzzle-sdk-jvm") {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            from(components["java"])
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", pomDesc)
+                    appendNode("name", rootProject.name)
+                    appendNode("url", pomUrl)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", pomLicenseName)
+                        appendNode("url", pomLicenseUrl)
+                        appendNode("distribution", pomLicenseDist)
+                    }
+                    appendNode("developers").appendNode("developer").apply {
+                        appendNode("id", pomDeveloperId)
+                        appendNode("name", pomDeveloperName)
+                    }
+                    appendNode("scm").apply {
+                        appendNode("url", pomScmUrl)
+                    }
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    publish = true
+
+    setPublications("kuzzle-sdk-jvm")
+
+    pkg.apply {
+        repo = "maven"
+        name = artifactName
+        userOrg = "kuzzle"
+        githubRepo = githubRepo
+        vcsUrl = pomScmUrl
+        description = "Kuzzle JVM SDK"
+        setLabels("kuzzle", "java", "kotlin", "scala", "jvm", "sdk")
+        setLicenses("MIT")
+        desc = description
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            released = LocalDateTime.now().toString()
+            vcsTag = artifactVersion
+        }
+    }
 }
 
 group = "io.kuzzle.sdk"
@@ -75,63 +161,4 @@ tasks.withType<Jar> {
         )
     }
     from(configurations.compileClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("MyPublication") {
-            from(components["java"])
-            artifact(sourcesJar)
-            artifact(dokkaJar)
-            groupId("io.kuzzle")
-            artifactId("kuzzle-sdk-jvm")
-            version(version)
-            pom.withXml {
-            def root = asNode()
-            root.appendNode("description", "Kuzzle JVM SDK")
-            root.appendNode("name", "kuzzle-sdk-java")
-            root.appendNode("url", "https://github.com/kuzzleio/sdk-jvm")
-            root.children().last() + pomConfig
-            }
-        }
-    }
-    repositories {
-        maven {
-            url = uri("$buildDir/repository")
-        }
-    }
-}
-
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_KEY")
-    publish = true
-    setPublications("MyPublication")
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "maven"
-        name = "kuzzle-sdk-jvm"
-        userOrg = "kuzzle"
-        websiteUrl = "https://kuzzle.io"
-        vcsUrl = "https://github.com/kuzzleio/sdk-jvm.git"
-        setLabels("kotlin")
-        desc = "Kuzzle JVM SDK"
-        publicDownloadNumbers = true
-        publish = true
-    })
-
-}
-
-tasks {
-    withType(GradleBuild::class.java) {
-        dependsOn(shadowJar)
-    }
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    withType(Test::class.java) {
-        testLogging.showStandardStreams = true
-    }
-    withType<GenerateMavenPom> {
-        destination = file("$buildDir/libs/${shadowJar.archiveName}.pom")
-    }
 }
