92c03ed3c01202271a7da077ceff4cfc7cf53285
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2020-11-27 14:02:32 +0100

    Update doc/1/getting-started/kotlin/snippets/document-kotlin.kt
    Co-authored-by: Alexandre Bouthinon <bouthinon.alexandre@gmail.com>

--- doc/1/getting-started/kotlin/snippets/document-kotlin.kt
+++ doc/1/getting-started/kotlin/snippets/document-kotlin.kt
@@ -8,7 +8,6 @@ fun main() {
   // Creates a WebSocket connection.
   // Replace "kuzzle" with
   // your Kuzzle host name (e.g. "localhost")
-
   val ws = WebSocket("kuzzle");
 
   // Instantiates a Kuzzle client
