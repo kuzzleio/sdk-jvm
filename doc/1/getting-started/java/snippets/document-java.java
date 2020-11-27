60ea453e2f81f814a572705a9abf22561749d092
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2020-11-27 14:02:05 +0100

    Update doc/1/getting-started/java/snippets/document-java.java
    Co-authored-by: Alexandre Bouthinon <bouthinon.alexandre@gmail.com>

--- doc/1/getting-started/java/snippets/document-java.java
+++ doc/1/getting-started/java/snippets/document-java.java
@@ -12,7 +12,6 @@ public class SnippetTest {
       // Creates a WebSocket connection.
       // Replace "kuzzle" with
       // your Kuzzle host name (e.g. "localhost")
-
       WebSocket ws = new WebSocket("kuzzle");
 
       // Instantiates a Kuzzle client
