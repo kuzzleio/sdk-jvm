18ce18b39e4f8e466a767401187fa66a645a4ed2
jenow <jenova.ke20@gmail.com>
2021-02-05 18:48:28 +0100

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 
 val artifactName = "sdk-jvm"
 val artifactGroup = "io.kuzzle"
-val artifactVersion = "1.0.3-"
+val artifactVersion = "1.0.3"
 
 val pomUrl = "https://github.com/kuzzleio/sdk-jvm"
 val pomScmUrl = "https://github.com/kuzzleio/sdk-jvm"
