2d67fd80db8bc4c726c72938e49b880e4a4dca25
Shiranuit <shiranui007@gmail.com>
2022-03-30 11:46:02 +0200

    simplify event manager

--- src/main/kotlin/io/kuzzle/sdk/events/EventManager.kt
+++ src/main/kotlin/io/kuzzle/sdk/events/EventManager.kt
@@ -1,9 +1,8 @@
 package io.kuzzle.sdk.events
 
 open class EventManager {
-    protected val listeners: HashMap<Any, Any> = HashMap()
+    private val listeners: HashMap<Any, Any> = HashMap()
 
-    // Java version of addListener function
     fun <T : Any> addListener(eventClass: Class<T>, listener: (T) -> Unit) {
         if (listeners[eventClass] == null) {
             listeners[eventClass] = mutableListOf(listener)
@@ -13,18 +12,17 @@ open class EventManager {
         }
     }
 
-    // Kotlin inlined version of addListener for better readability and usage
-    inline fun <reified T : Any> addListener(noinline listener: (T) -> Unit) {
-        addListener(T::class.java, listener)
-    }
-
-    // Kotlin version of trigger function, no need for Java version since
-    internal inline fun <reified T : Any> trigger(event: T) {
-        if (listeners[T::class.java] != null) {
-            var invokerList = listeners[T::class.java] as MutableList<(T) -> Unit>
+    fun <T : Any> trigger(event: T) {
+        if (listeners[event.javaClass] != null) {
+            var invokerList = listeners[event.javaClass] as MutableList<(T) -> Unit>
             for (invokable: (T) -> Unit in invokerList) {
                 invokable.invoke(event)
             }
         }
+    }
+
+    // Kotlin inlined version of addListener for better readability and usage
+    inline fun <reified T : Any> addListener(noinline listener: (T) -> Unit) {
+        addListener(T::class.java, listener)
     }
 }
