1ba29bee9a2daed801eab917f51c46c8cc3a4bc2
Shiranuit <shiranui007@gmail.com>
2022-05-17 15:45:19 +0200

    bump SDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 val artifactName = "sdk-jvm"
 val artifactGroup = "io.kuzzle"
-val artifactVersion = "1.3.0"
+val artifactVersion = "1.3.1"
 
 val pomUrl = "https://github.com/kuzzleio/sdk-jvm"
 val pomScmUrl = "https://github.com/kuzzleio/sdk-jvm"
